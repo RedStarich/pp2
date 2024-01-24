2c2b4a82c42d7aa294a805edb0e6cc6b7dd9840d
Altair <altairzhambyl@gmail.com>
2024-01-24 12:21:56 +0600

    modules task finished

---
+++ lab3/modules.py
@@ -0,0 +1,13 @@
+#ex1
+'''import mymodule''' #commented to avoid errors due to non-existent mymodule
+
+#ex2
+'''import mymodule as mx'''
+
+#ex3
+'''import mymodule
+print(dir(mymodule))'''
+
+#ex4
+
+'''from mymodule import person1'''
