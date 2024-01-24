432b9ae2b83135f34235e067266bf4c931c02ab7
Altair <altairzhambyl@gmail.com>
2024-01-24 12:44:04 +0600

    3rd task solved

--- lab3/fun1_kbtu.py
+++ lab3/fun1_kbtu.py
@@ -1,14 +1,38 @@
-#task1
-class Solution:
-    grams = input("enter mass in grams")
-    def to_ounces(grams):
-        ounces = 28.3495231 * grams
-        return ounces
+# #task1
+# class Solution:
+#     grams = input("enter mass in grams")
+#     def to_ounces(grams):
+#         ounces = 28.3495231 * grams
+#         return ounces
 
-#task2
-class Solution:
-    F = input("Enter temperature in Fahrenheit")
-    def Fahrenheit_to_Celsius(F):
-        C = (5 / 9) * (F - 32)
+# #task2
+# class Solution:
+#     F = input("Enter temperature in Fahrenheit")
+#     def Fahrenheit_to_Celsius(F):
+#         C = (5 / 9) * (F - 32)
+#         return C
 
+#task3
+class Solution:
+    heads = 35
+    legs = 94
     
+    def solve(heads, legs):
+        rabbits = 0
+        chickens = 0
+        chickens = 2 * heads - 0.5 * legs
+        '''
+            heads = rabbits + chickens
+            4 * rabbits = (legs - 2*chickens)
+            rabbits = 1/4(legs - 2*chickens)
+            rabbits = heads - chickens
+            heads - chickens = 1/4 (legs - 2*chickens)
+            chickens = heads - (1/4)legs + (1/2)chickens
+            (1/2)chickens = heads - (1/4)legs
+            chickens = 2*heads - (1/2)legs
+        '''
+        rabbits = heads - chickens
+        print("rabbit count = ", rabbits, "chicken count = ", chickens)
+
+    solve(heads, legs)
+
