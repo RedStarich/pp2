e5f2934560df200531273d75175662295bf2e612
Altair <altairzhambyl@gmail.com>
2024-01-24 12:45:14 +0600

    uncommented the tasks

--- lab3/fun1_kbtu.py
+++ lab3/fun1_kbtu.py
@@ -1,16 +1,16 @@
-# #task1
-# class Solution:
-#     grams = input("enter mass in grams")
-#     def to_ounces(grams):
-#         ounces = 28.3495231 * grams
-#         return ounces
+#task1
+class Solution:
+    grams = input("enter mass in grams")
+    def to_ounces(grams):
+        ounces = 28.3495231 * grams
+        return ounces
 
-# #task2
-# class Solution:
-#     F = input("Enter temperature in Fahrenheit")
-#     def Fahrenheit_to_Celsius(F):
-#         C = (5 / 9) * (F - 32)
-#         return C
+#task2
+class Solution:
+    F = input("Enter temperature in Fahrenheit")
+    def Fahrenheit_to_Celsius(F):
+        C = (5 / 9) * (F - 32)
+        return C
 
 #task3
 class Solution:
