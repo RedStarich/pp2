daf5f164ae02293e6b2d78af2e01dba5a2f09830
Altair <altairzhambyl@gmail.com>
2024-02-21 10:40:09 +0600

    json task

---
+++ lab4/parse.py
@@ -0,0 +1,24 @@
+import json
+
+with open('sample-data.json') as f:
+    data = json.load(f)
+
+imdata = data["imdata"]
+
+phys = ["l1PhysIf"]
+
+print("{:<50} {:<20} {:<8} {:<6}".format("DN", "Description", "Speed", "MTU"))
+print("=" * 80)
+
+for entry in imdata[:3]:
+    count = 0
+    for obj_name in phys:
+        attr = entry[obj_name]["attributes"]
+        print("{:<50} {:<20} {:<8} {:<6}".format(
+            attr["dn"],
+            attr.get("descr", ""),
+            attr.get("speed", ""),
+            attr.get("mtu", "")
+        ))
+        break
+    
