2d5845092b38f9272ac606e588592c934984e2dc
Altair <altairzhambyl@gmail.com>
2024-02-28 10:48:33 +0600

    regex

--- lab5/reg.py
+++ lab5/reg.py
@@ -1,9 +1,31 @@
 import re
-
+#1
 string = input("Enter the sentence: ")
-x = re.search(r"ab*", string)
+# x = re.compile(r"ab*")
 
-if not x:
-    print("no")
-else:
-    print("yes")
+#2
+# x = re.compile(r"ab{2,3}")
+
+#3
+# x = re.compile(r"[a-z]_[a-z]")
+
+#4
+# x = re.compile(r"[A-Z][a-z]")
+
+#5
+# x = re.compile(r"a[a-z]*b$")          #uncomment the code to run
+
+# print(x.findall(string))              #uncomment this line to run tasks 1-5
+
+#6
+# x = re.sub(r'[\s, \.]', string=string, repl=":")
+#7
+# x = re.split('_+', string)
+# x = x[0].lower() + "".join(map(lambda a: a.title(), x[1:]))
+#8
+x = re.split("[A-Z]", string)
+x = " ".join(x)
+#9
+x = re.split("[A-Z]", string)
+x = " ".join(x)
+print(x)
